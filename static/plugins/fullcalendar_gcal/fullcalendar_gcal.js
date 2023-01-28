5ad8cb43b19b8ed7de58bb41314e3d6e5ac66cee
gyufi <gyufi@sztaki.hu>
2023-01-28 11:38:46 +0100

    fix(fullcalendar): fullcalendar js hibák javítása

--- static/plugins/fullcalendar_gcal/fullcalendar_gcal.js
+++ static/plugins/fullcalendar_gcal/fullcalendar_gcal.js
@@ -2,22 +2,24 @@
 
  $(document).ready(function() {
   var calendarEl = document.getElementById('calendar');
-  var calendar = new FullCalendar.Calendar( calendarEl, {
-    initialView: 'dayGridMonth',
-    googleCalendarApiKey: 'AIzaSyBs_iHXnsg_-srQoaqm__SxO3v2o_LD1Zw',
-    eventSources: [
-      {
-        googleCalendarId: 'mkse3rucalq83d1c3hmijufr1o@group.calendar.google.com',
-        className: 'iskola-event'
-      },
-      {
-        googleCalendarId: '2qi0h9h72954mbg2mt92etq2fc@group.calendar.google.com',
-        className: 'alapitvany-event'
-      }
-    ],
-    themeSystem: 'bootstrap',
-    locale: 'hu',
-    firstDay: 1
-  });
-  calendar.render();
+  if (calendarEl !== null) {
+    var calendar = new FullCalendar.Calendar( calendarEl, {
+      initialView: 'dayGridMonth',
+      googleCalendarApiKey: 'AIzaSyBs_iHXnsg_-srQoaqm__SxO3v2o_LD1Zw',
+      eventSources: [
+        {
+          googleCalendarId: 'mkse3rucalq83d1c3hmijufr1o@group.calendar.google.com',
+          className: 'iskola-event'
+        },
+        {
+          googleCalendarId: '2qi0h9h72954mbg2mt92etq2fc@group.calendar.google.com',
+          className: 'alapitvany-event'
+        }
+      ],
+      themeSystem: 'bootstrap',
+      locale: 'hu',
+      firstDay: 1
+    });
+    calendar.render();
+};
 });
